f656a716682cd10d90d6d2d07657f1619f90cc09
0x7c00 <zhupengchengzpc@gmail.com>
2021-12-26 16:34:16 +0800

    add cmake clean work

--- install.sh
+++ install.sh
@@ -1,3 +1,12 @@
 #!/usr/bin/env bash
+n_threads=$(nproc)
+echo "supporting threads number: " $n_threads
+if [ -d "./cgit/build" ];
+then 
+    echo "do cmake clean work, delete cgit/build directory"
+    rm -r -f ./cgit/build
+else 
+    ehco ""
+fi
 mkdir cgit/build && cd cgit/build && cmake ..
-make -j8 && sudo make install
+make -j$n_threads && sudo make install
